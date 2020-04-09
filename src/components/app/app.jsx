d0abfa8c64db12a5dbbe42832d1a57e0b8c24ab5
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-04-09 10:18:49 +0300

    Implements deleted item.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 
 import AppHeader from '../app-header/app-header';
 import SearchPanel from '../search-panel/search-panel';
@@ -7,25 +7,41 @@ import ItemStatusFilter from '../item-status-filter/item-status-filter';
 
 import './app.css';
 
-const App = () => {
+export default class App extends PureComponent {
 
-  const todoData = [
-    {label: `Drink Coffee`, important: false, id: 1},
-    {label: `Make Awesome App`, important: true, id: 2},
-    {label: `Have a lunch`, important: false, id: 3},
-  ];
+  state = {
+    todoData: [
+      {label: `Drink Coffee`, important: false, id: 1},
+      {label: `Make Awesome App`, important: true, id: 2},
+      {label: `Have a lunch`, important: false, id: 3},
+    ],
+  };
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3}/>
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+  _handleDeletedItemClick = (id) => {
+
+    this.setState((prevState) => {
+      const deletedIndexItem = prevState.todoData.findIndex((it) => it.id === id);
+      const newData = [
+        ...prevState.todoData.slice(0, deletedIndexItem),
+        ...prevState.todoData.slice(deletedIndexItem + 1)
+      ];
+
+      return {todoData: newData};
+    });
+  };
+
+  render() {
+    const {todoData} = this.state;
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3}/>
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+
+        <TodoList todoData={todoData} onDeleted={this._handleDeletedItemClick} />
       </div>
-
-      <TodoList todoData={todoData} />
-    </div>
-  );
-};
-
-export default App;
+    );
+  }
+}
--- src/components/todo-list-item/todo-list-item.jsx
+++ src/components/todo-list-item/todo-list-item.jsx
@@ -1,33 +1,54 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import './todo-list-item.css';
 
-const TodoListItem = ({label, important}) => {
 
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
+export default class TodoListItem extends PureComponent {
+  state = {
+    done: false,
+    important: false,
   };
 
-  return (
-    <span className="todo-list-item">
-      <span
-        className="todo-list-item-label"
-        style={style}>
-        {label}
+  _handleLabelClick = () => {
+    this.setState((prevState) => ({done: !prevState.done}));
+  };
+
+  _handleImportantBtnClick = () => {
+    this.setState((prevState) => ({important: !prevState.important}));
+  };
+
+  render() {
+    const {label, onDeleted} = this.props;
+    const {done, important} = this.state;
+
+    const classNames = `todo-list-item ${done ? `done` : ``} ${important ? `important` : ``}`;
+
+    const style = {
+      color: important ? `steelblue` : `black`,
+      fontWeight: important ? `bold` : `normal`
+    };
+
+    return (
+      <span className={classNames}>
+        <span
+          className="todo-list-item-label"
+          style={style}
+          onClick={this._handleLabelClick}>
+          {label}
+        </span>
+
+        <button type="button"
+                className="btn btn-outline-success btn-sm float-right"
+                onClick={this._handleImportantBtnClick}>
+          <i className="fa fa-exclamation" />
+        </button>
+
+        <button type="button"
+                className="btn btn-outline-danger btn-sm float-right"
+                onClick={onDeleted}>
+          <i className="fa fa-trash-o" />
+        </button>
       </span>
-
-      <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation" />
-      </button>
-
-      <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o" />
-      </button>
-    </span>
     );
-};
-
-export default TodoListItem;
+  }
+}
 
